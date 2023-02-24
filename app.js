bd8e79b1b71d7dd7c44b024508d76396164416dc
aminhl <amin.hlel@esprit.tn>
2023-02-24 22:21:14 +0100

    global_error_handling_middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -13,5 +14,14 @@ app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  /* const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.statusCode = 404;
+  err.status = 'fail';*/
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 400));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
